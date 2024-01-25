d10975a10c18ea904b152d5bc82177e8a3527687
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-25 17:17:20 -0300

    Aplicando condicao ao html com NgIf

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componente Filho',
-      modelo: 'modelo3'
-    }
+    // {
+    //   conteudo: 'Passo informações para o componente filho',
+    //   autoria: 'Componente Pai',
+    //   modelo: 'modelo2'
+    // },
+    // {
+    //   conteudo: 'Minha propriedade é decorada com @Input()',
+    //   autoria: 'Componente Filho',
+    //   modelo: 'modelo3'
+    // }
   ];
 
   constructor() { }
