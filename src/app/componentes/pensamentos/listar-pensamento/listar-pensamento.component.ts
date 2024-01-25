a6d05287f6a9a258c771771dce048f9d390c6e03
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-25 16:49:47 -0300

    Comunicacao entre componente Pai e Filho

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente Filho',
+      modelo: 'modelo3'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
+  //Este decorator é utilizado para dizer que este objeto será alimentado por um componente Pai, no nosso caso o Pai
+  //é o componente ListarPensamentos. Então no html deste componente, nós setamos o objeto quei irá alimenta-lo
+  //através de PropertyBinding. Consequentemente os valores que estão definidos agora serão sobrescritos.
+  @Input()
   pensamento = {
     conteudo: 'I Love Angular',
     autoria: 'Jorge Rufino',
     modelo: 'modelo3'
-  }
+  };
 
   constructor() { }
 
