3768982a334a72216fbe897bf47a38a16171623b
jorge-rufino <jorge.rufino1986@gmail.com>
2024-01-25 10:51:43 -0300

    Component ListarPensamento, Navegacao dinamica e redirecionamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',                                //http://localhost:4200/
+    redirectTo: 'listarPensamento',         //Quando for acessado a rota acima, irá redirecionar para o Mural
+    pathMatch: 'full'
+  },
+  {
+  path:'criarPensamento',                   //http://localhost:4200/criarPensamento
+    component: CriarPensamentoComponent
+  },
+  {                                         //Mural
+    path:'listarPensamento',                //http://localhost:4200/listarPensamento
+    component: ListarPensamentoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
