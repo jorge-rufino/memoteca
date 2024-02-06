97aec39a7ac5a5634defef2064ac5ae6433fb248
jorge-rufino <jorge.rufino1986@gmail.com>
2024-02-06 10:07:25 -0300

    Tipando formBuilder.group e Pattern de Autoria

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -5,12 +5,20 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { minusculoValidator } from 'src/app/validators/minusculoValidators';
 
+interface MeuFormulario {
+  conteudo: any;
+  autoria: any;
+  modelo: any;
+}
+
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
+
+
 
   formulario!: FormGroup;
 
@@ -21,7 +29,7 @@ export class CriarPensamentoComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.formulario = this.formBuilder.group({
+    this.formulario = this.formBuilder.group<MeuFormulario>({
       conteudo: ['', Validators.compose([
         Validators.required,
         Validators.pattern(/(.|\s)*\S(.|\s)*/)    //Não permite salvar usando somente espaços em branco
@@ -30,7 +38,7 @@ export class CriarPensamentoComponent implements OnInit {
           Validators.required,
           Validators.minLength(3),
           Validators.maxLength(50),
-          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+          Validators.pattern(/\S.*\S/),           //Aceita somente 1 espaço em branco entre caracteres
           minusculoValidator
         ]
       ],
@@ -39,7 +47,9 @@ export class CriarPensamentoComponent implements OnInit {
   }
 
   criarPensamento() {
-    console.log(this.formulario.get('conteudo')?.errors);   //Mostra os erros dos validators
+    //console.log(this.formulario.get('conteudo')?.errors);   //Mostra os erros dos validators
+    this.formulario.controls['autoria'].setValue(this.formulario.controls['autoria'].value.trim());
+
     if(this.formulario.valid){
       this.pensamentoService.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento']);
